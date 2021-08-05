1be1419f5a349df2b2148576072e2aa4285b0460
SaralaBashipaka <Mallesh@LAPTOP-UAVHLRUC>
2021-08-05 12:37:51 +0530

    WH-47-fourth-commit

--- src/main/java/ni/nareshit/raghu/runner/MessageRunner.java
+++ src/main/java/ni/nareshit/raghu/runner/MessageRunner.java
@@ -1,11 +1,15 @@
 package ni.nareshit.raghu.runner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageRunner implements CommandLineRunner {
 
+	private static Logger log=LoggerFactory.getLogger(MessageRunner.class);
+	
 	public void run(String... args) throws Exception {
         int a= 10;
 		System.out.println("Hello");
