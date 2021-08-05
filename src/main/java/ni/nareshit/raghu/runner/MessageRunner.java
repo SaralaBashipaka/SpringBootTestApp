d8a9e3fd8a72688ef63f719bfe3bf8d96101746b
SaralaBashipaka <Mallesh@LAPTOP-UAVHLRUC>
2021-08-05 12:06:51 +0530

    WH-8520- First Time File Update

---
+++ src/main/java/ni/nareshit/raghu/runner/MessageRunner.java
@@ -0,0 +1,14 @@
+package ni.nareshit.raghu.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageRunner implements CommandLineRunner {
+
+	public void run(String... args) throws Exception {
+          System.out.println("Hello");
+		
+	}
+
+}
